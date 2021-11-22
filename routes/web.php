ce67e076e69fd781705aac2fa34aea5ee826953e
nmq126 <minhquangngo.98@gmail.com>
2021-11-22 22:04:08 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,11 @@ Route::get('/cart/add',[ShoppingCartController::class, 'add']);
 Route::get('/cart/show',[ShoppingCartController::class, 'show']);
 Route::get('/cart/remove',[ShoppingCartController::class, 'remove']);
 Route::post('/cart/update',[ShoppingCartController::class, 'update']);
+
+
+Route::get('/sign_in', function () {
+    return view('client.sign_in');
+});
+Route::get('/sign_up', function () {
+    return view('client.sign_up');
+});
