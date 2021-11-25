ac33af2d94a2f686f71aad2e4f86c276755474b1
Trinh Ngoc Huy <huytnth2010022@fpt.edu.vn>
2021-11-25 21:12:01 +0700

    product_detail_1.2.2

--- resources/views/client/product_detail/product_detail.blade.php
+++ resources/views/client/product_detail/product_detail.blade.php
@@ -5,13 +5,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="/user/css/product_detail.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="http://localhost/client_web/css/product_detail.css">
+
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer"
         />
         <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=618600ef20e65f001281c8c1&product=sticky-share-buttons" async="async"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <!-- Google Fonts -->
         <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
         <script>
@@ -72,10 +74,10 @@
         <button>Add To Cart</button>
         <button>💗</button>
         <div class="quantity">
-        <button>-</button>
-        <span id="quantity">1</span>
-        <input type="hidden">
-        <button>+</button>
+        <button name="minus">-</button>
+        <span id="quantity-span">1</span>
+        <input name="quantity" type="hidden">
+        <button name="plus">+</button>
         </div>
         </div>
         <div class="category">
@@ -117,27 +119,72 @@
         </div>
         <div class="main2">
         <div class="container">
-        <div class="detail">
-        <h2>Detail</h2>
-        <div class="detail-content .col-md-4">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Mañana, a partir de los botones de opción, sino un poco de dolor. Escribir en el freno ahora. No hay comentarios eran, en el mejor, pero el financiamiento
-        de la atención de la salud o la risa. Bienvenido a aprender más acerca de la cultura popular, y el tiempo, no, ni a crecer fuerte. Miramos a los miembros de televisión por cable de televisión.</p>
+        <div class="title">
+        <div class="detail active">
+        Detail
+        </div>
+        <div class="comment ">
+        Comment
+        </div>
+        </div>
+        <div class="detail-content">
+
+        <div class="detail-item-1">
+
+        <div class="detail-content container mt-5">
+        <div class="d-flex justify-content-center row">
+        <div class="col-md-8">
+        <div class="d-flex flex-column comment-section">
+        <div class="bg-white p-2">
+        <div class="d-flex flex-row user-info"><img class="rounded-circle" src="https://res-console.cloudinary.com/du0vz0npz/thumbnails/v1/image/upload/v1637841329/UWJfcGZqdDF1/preview" width="40">
+        <div class="d-flex flex-column justify-content-start ml-2"><span class="d-block font-weight-bold name">Ngô Minh Quang</span><span class="date text-black-50">Shared publicly - Jan 2020</span></div>
+        </div>
+        <div class="mt-2">
+        <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat.</p>
+        </div>
+        </div>
+        <div class="bg-white">
+        <div class="d-flex flex-row fs-12">
+        <div class="like p-2 cursor"><i class="fa fa-thumbs-o-up"></i><span class="ml-1">Like</span></div>
+        <div class="like p-2 cursor"><i class="fa fa-commenting-o"></i><span class="ml-1">Comment</span></div>
+        <div class="like p-2 cursor"><i class="fa fa-share"></i><span class="ml-1">Share</span></div>
+        </div>
+        </div>
+        <div class="bg-light p-2">
+        <div class="d-flex flex-row align-items-start"><img class="rounded-circle" src="https://i.pinimg.com/236x/94/12/c3/9412c35b4e74c5510cb6aa6beed473fa.jpg" width="40" height="40"><textarea class="form-control ml-1 shadow-none textarea"></textarea></div>
+        <div class="mt-2 text-right"><button class="btn btn-primary btn-sm shadow-none" type="button">Post comment</button><button class="btn btn-outline-primary btn-sm ml-1 shadow-none" type="button">Cancel</button></div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        <div class="detail-item-2">
+
+        <div class="detail-content container mt-5">
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Mañana, a partir de los botones de opción, sino un poco de dolor. Escribir en el freno ahora. No hay comentarios eran, en el mejor, pero el
+        financiamiento de la atención de la salud o la risa. Bienvenido a aprender más acerca de la cultura popular, y el tiempo, no, ni a crecer fuerte. Miramos a los miembros de televisión por cable de televisión.</p>
         <div class="img"> <img src="https://burgerking.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/d/b/dbl_crunchy_whp-min_1.jpg" alt=""></div>
-        <p>Por otra parte, denunciamos con indignación a los hombres que son engañados y desmoralizados por los encantos del placer del momento, tan cegados por el deseo, que no pueden prever el dolor y la molestia que se va a producir, y
-        la misma culpa es de los que faltan a su deber por debilidad de la voluntad, que es lo mismo que decir que fallan por la fatiga y el dolor. Estos casos son muy simples y fácil de distinguir. En una hora libre, sin las trabas
-        de nuestro poder de elección y cuando nada impida que seamos capaces de hacer lo que más nos gusta, todo placer es de agradecer y cada dolor se puede evitar. Pero en ciertas circunstancias y debido a las exigencias del deber
-    o de las obligaciones de la empresa, estos placeres tienen que ser repudiados y sus molestias aceptadas .El hombre sabio, por lo tanto, siempre tiene en estos asuntos una elección: rechaza placeres para asegurar otros placeres
-        mayores, o de lo contrario evita los dolores para evitar dolores peores.</p>
+        <p>Por otra parte, denunciamos con indignación a los hombres que son engañados y desmoralizados por los encantos del placer del momento, tan cegados por el deseo, que no pueden prever el dolor y la molestia que se va a producir,
+        y la misma culpa es de los que faltan a su deber por debilidad de la voluntad, que es lo mismo que decir que fallan por la fatiga y el dolor. Estos casos son muy simples y fácil de distinguir. En una hora libre, sin las
+        trabas de nuestro poder de elección y cuando nada impida que seamos capaces de hacer lo que más nos gusta, todo placer es de agradecer y cada dolor se puede evitar. Pero en ciertas circunstancias y debido a las exigencias
+    del deber o de las obligaciones de la empresa, estos placeres tienen que ser repudiados y sus molestias aceptadas .El hombre sabio, por lo tanto, siempre tiene en estos asuntos una elección: rechaza placeres para asegurar
+        otros placeres mayores, o de lo contrario evita los dolores para evitar dolores peores.</p>
         </div>
 
+
         </div>
+        </div>
+
+
         </div>
         <div class="related">
         <div class="header">
         Related Product
         </div>
+        <div class="cart-wrapper">
         <div class="carts">
-
         <div class="cart-item">
         <div class="img-cart">
         <div class="discount">
@@ -145,7 +192,7 @@
         </div>
         <img src="https://cdn.shopify.com/s/files/1/0037/1818/5030/products/4_7aea6bc7-c789-4c79-a472-b63f4e77e0d8_large.jpg?v=1536551762" alt="">
         <div class="add-cart">
-        <i class="fas fa-shopping-cart"></i> <button>Add to cart</button>
+        <i class="fas fa-shopping-cart"></i> <span> Add to cart</span>
         </div>
         </div>
         <div class="info-cart">
@@ -153,18 +200,85 @@
         <div class="name">Leyendo Distintos</div>
         <div class="price">$585.00</div>
         </div>
-
+        </div>
+        </div>
+        <div class="cart-item">
+        <div class="img-cart">
+        <div class="discount">
+        <span> 13%</span>
+        </div>
+        <img src="https://cdn.shopify.com/s/files/1/0037/1818/5030/products/4_7aea6bc7-c789-4c79-a472-b63f4e77e0d8_large.jpg?v=1536551762" alt="">
+        <div class="add-cart">
+        <i class="fas fa-shopping-cart"></i> <span> Add to cart</span>
+        </div>
+        </div>
+        <div class="info-cart">
+        <div class="name-price">
+        <div class="name">Leyendo Distintos</div>
+        <div class="price">$585.00</div>
+        </div>
+        </div>
+        </div>
+        <div class="cart-item">
+        <div class="img-cart">
+        <div class="discount">
+        <span> 13%</span>
+        </div>
+        <img src="https://cdn.shopify.com/s/files/1/0037/1818/5030/products/4_7aea6bc7-c789-4c79-a472-b63f4e77e0d8_large.jpg?v=1536551762" alt="">
+        <div class="add-cart">
+        <i class="fas fa-shopping-cart"></i> <span> Add to cart</span>
+        </div>
+        </div>
+        <div class="info-cart">
+        <div class="name-price">
+        <div class="name">Leyendo Distintos</div>
+        <div class="price">$585.00</div>
+        </div>
+        </div>
+        </div>
+        <div class="cart-item">
+        <div class="img-cart">
+        <div class="discount">
+        <span> 13%</span>
+        </div>
+        <img src="https://cdn.shopify.com/s/files/1/0037/1818/5030/products/4_7aea6bc7-c789-4c79-a472-b63f4e77e0d8_large.jpg?v=1536551762" alt="">
+        <div class="add-cart">
+        <i class="fas fa-shopping-cart"></i> <span> Add to cart</span>
+        </div>
+        </div>
+        <div class="info-cart">
+        <div class="name-price">
+        <div class="name">Leyendo Distintos</div>
+        <div class="price">$585.00</div>
+        </div>
+        </div>
+        </div>
+        <div class="cart-item">
+        <div class="img-cart">
+        <div class="discount">
+        <span> 13%</span>
+        </div>
+        <img src="https://cdn.shopify.com/s/files/1/0037/1818/5030/products/4_7aea6bc7-c789-4c79-a472-b63f4e77e0d8_large.jpg?v=1536551762" alt="">
+        <div class="add-cart">
+        <i class="fas fa-shopping-cart"></i> <span> Add to cart</span>
+        </div>
+        </div>
+        <div class="info-cart">
+        <div class="name-price">
+        <div class="name">Leyendo Distintos</div>
+        <div class="price">$585.00</div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
         </div>
 
-        </div>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-        </div>
-        </div>
-        </div>
-        </div>
-
-
-        <script src="/user/js/product_detail.js"></script>
+        <script src="http://localhost/client_web/js/product_detail.js"></script>
         </body>
 
         </html>
