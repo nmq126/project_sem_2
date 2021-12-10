44122aa788421bf6d2ea51fcbe9d27e247c92006
nmq126 <minhquangngo.98@gmail.com>
2021-12-11 00:26:39 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,10 @@ class   AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         view()->composer('*',function ($view){
 
             $shoppingCart = [];
