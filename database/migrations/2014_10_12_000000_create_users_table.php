d0fd5e5007ebe125792530672006283830b15d3c
nmq126 <minhquangngo.98@gmail.com>
2021-12-10 23:26:29 +0700

    test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class   CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->string('password');
