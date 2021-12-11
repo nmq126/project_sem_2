d4f6a7d904bb48abc2f773f1b693cd0c3c90e1ba
nmq126 <minhquangngo.98@gmail.com>
2021-12-11 13:17:42 +0700

    back mtfk up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class   AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        // dùng asset load bị lỗi do http
         if(config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
