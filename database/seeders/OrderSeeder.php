760c7abfbac6c1b98e4bd0e6f292853ad877072e
Ngô Minh Quang <48945882+nmq126@users.noreply.github.com>
2021-12-14 16:58:34 +0700

    Update OrderSeeder.php

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -22,7 +22,7 @@ class OrderSeeder extends Seeder
 
 
 
-               [
+              [
                     'id' => '1',
                     'user_id' => '2',
                     'ship_name'=> 'Quang',
@@ -47,6 +47,62 @@ class OrderSeeder extends Seeder
                     'checkout'=> '1',
                     'status' => '4',
                     'created_at'=>"2021-09-24 13:22:25.000000",
+
+                ],
+
+                [
+                    'id' => '3',
+                    'user_id' => '4',
+                    'ship_name'=> 'Quốc Hưng',
+                    'ship_phone'=> '045896545',
+                    'ship_address' => '360 Xã Đàn',
+                    'ship_note' => 'không hành',
+                    'total_price' => 103500,
+                    'checkout'=> '0',
+                    'status' => '4',
+                    'created_at'=>"2021-09-25 18:20:27.000000",
+
+                ],
+
+                [
+                    'id' => '4',
+                    'user_id' => '2',
+                    'ship_name'=> 'bạn Quang',
+                    'ship_phone'=> '0855566898',
+                    'ship_address' => 'ngõ chợ Khâm Thiên',
+                    'ship_note' => '',
+                    'total_price' => 148500,
+                    'checkout'=> '0',
+                    'status' => '4',
+                    'created_at'=>"2021-09-25 20:22:37.000000",
+
+                ],
+
+                [
+                    'id' => '5',
+                    'user_id' => '3',
+                    'ship_name'=> 'Huy cầu giấy',
+                    'ship_phone'=> '01254548965',
+                    'ship_address' => '94 Cầu Giấy',
+                    'ship_note' => '',
+                    'total_price' => 94500,
+                    'checkout'=> '1',
+                    'status' => '4',
+                    'created_at'=>"2021-09-26 10:55:55.000000",
+
+                ],
+
+                [
+                    'id' => '6',
+                    'user_id' => '3',
+                    'ship_name'=> 'Huy cầu giấy',
+                    'ship_phone'=> '01254548965',
+                    'ship_address' => '94 Cầu Giấy',
+                    'ship_note' => '',
+                    'total_price' => 180000,
+                    'checkout'=> '1',
+                    'status' => '4',
+                    'created_at'=>"2021-09-26 19:41:41.000000",
 
                 ],
 
