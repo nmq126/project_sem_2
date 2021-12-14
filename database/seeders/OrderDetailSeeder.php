03f26c7d3c778374f4eb4411ce2d196d02870796
Ngô Minh Quang <48945882+nmq126@users.noreply.github.com>
2021-12-14 16:58:08 +0700

    Update OrderDetailSeeder.php

--- database/seeders/OrderDetailSeeder.php
+++ database/seeders/OrderDetailSeeder.php
@@ -17,7 +17,7 @@ class OrderDetailSeeder extends Seeder
 //        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         DB::table('order_details')->truncate();
         DB::table('order_details')->insert([
-            [
+           [
                 'product_id' => '1',
                 'order_id' => '1',
                 'quantity' => '3',
@@ -50,6 +50,69 @@ class OrderDetailSeeder extends Seeder
                 'order_id' => '2',
                 'quantity' => '1',
                 'unit_price' => 58500,
+            ],
+
+            [
+                'product_id' => '13',
+                'order_id' => '3',
+                'quantity' => '2',
+                'unit_price' => 27000,
+            ],
+
+            [
+                'product_id' => '14',
+                'order_id' => '3',
+                'quantity' => '1',
+                'unit_price' => 22500,
+            ],
+
+            [
+                'product_id' => '15',
+                'order_id' => '3',
+                'quantity' => '1',
+                'unit_price' => 27000,
+            ],
+
+            [
+                'product_id' => '10',
+                'order_id' => '4',
+                'quantity' => '1',
+                'unit_price' => 58500,
+            ],
+
+            [
+                'product_id' => '11',
+                'order_id' => '4',
+                'quantity' => '1',
+                'unit_price' => 90000,
+            ],
+
+            [
+                'product_id' => '17',
+                'order_id' => '5',
+                'quantity' => '1',
+                'unit_price' => 40500,
+            ],
+
+            [
+                'product_id' => '9',
+                'order_id' => '5',
+                'quantity' => '1',
+                'unit_price' => 54000,
+            ],
+
+            [
+                'product_id' => '33',
+                'order_id' => '6',
+                'quantity' => '2',
+                'unit_price' => 49500,
+            ],
+
+            [
+                'product_id' => '34',
+                'order_id' => '6',
+                'quantity' => '2',
+                'unit_price' => 40500,
             ],
         ]);
 //        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
