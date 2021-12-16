e0a008f4d20cb45d6544ca51f85a39ab4f6ecd56
trinhngochuy <93603144+trinhngochuy@users.noreply.github.com>
2021-12-16 11:41:58 +0700

    Update user.js
    as

--- public/assets/js/user/user.js
+++ public/assets/js/user/user.js
@@ -1,31 +1,24 @@
-$(".delete").click(function(event) {
-    event.preventDefault();
-    deleteItem();
-});
-
 $("#cancel-item").click(function() {
     hideItem();
 });
 
-function deleteItem() {
-    $("#delete_order").slideDown();
+function deleteItem(id) {
+    $("#delete_order_" + id).slideDown();
+
 }
 
-function hideItem() {
-    $("#delete_order").slideUp();
+function hideItem(id) {
+    $("#delete_order_" + id).slideUp();
 }
-
 $("select[name=level]").change(function(e) {
-    if (window.confirm('Bạn có chắc muốn thay đổi chức vụ của user này?')){
+    if (window.confirm('Bạn có chắc muốn thay đổi chức vụ của user này?')) {
         this.form.submit();
-    }
-    else if (this.value == 1) this.value = 0;
+    } else if (this.value == 1) this.value = 0;
     else if (this.value == 0) this.value = 1;
 });
 $("select[name=status]").change(function(e) {
     if (window.confirm('Bạn có chắc muốn khóa/mở khóa tài khoản này?')) {
         this.form.submit();
-    }
-    else if (this.value == 1) this.value = 0;
+    } else if (this.value == 1) this.value = 0;
     else if (this.value == 0) this.value = 1;
 });
