f1ca2afb48336c646b43033f53aeeecef995d29a
trinhngochuy <93603144+trinhngochuy@users.noreply.github.com>
2021-12-16 11:35:53 +0700

    Update user_detail.js
    as

--- public/assets/js/user/user_detail.js
+++ public/assets/js/user/user_detail.js
@@ -25,3 +25,16 @@ $("input[name=key]").keyup(function(e) {
     }
 
 })
+
+$("#cancel-item").click(function() {
+    hideItem();
+});
+
+function deleteItem(id) {
+    $("#delete_order_" + id).slideDown();
+
+}
+
+function hideItem(id) {
+    $("#delete_order_" + id).slideUp();
+}
